be6f1d55a2099e4fb3338480efd7d3768a6cea0b
Side-On <nathanhighams@gmail.com>
2021-04-03 15:00:12 +0000

    Add register.html template to register function

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -28,6 +29,11 @@ def get_stories():
 def get_jokes():
     jokes = mongo.db.jokes.find()
     return render_template("jokes.html", jokes=jokes)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
