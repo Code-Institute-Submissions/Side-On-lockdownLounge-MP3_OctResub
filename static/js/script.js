f3ea89ea1163d24a30b55e2a4fff2c00bbb31fed
Side-On <nathanhighams@gmail.com>
2021-04-10 12:14:15 +0000

    Add action button to stories card allowing users to delete or edit their story

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,11 @@
     $('.datepicker').datepicker();
     $('.collapsible').collapsible();
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
