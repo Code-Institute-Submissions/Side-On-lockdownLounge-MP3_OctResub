0082fca2b8999697706c777ef043a285daae29df
Side-On <nathanhighams@gmail.com>
2021-04-20 21:07:02 +0000

    Delete space between functions

--- app.py
+++ app.py
@@ -149,7 +149,6 @@ def search():
     return render_template("stories.html", stories=stories)
 
 
-
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
