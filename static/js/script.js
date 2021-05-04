4c4db656ab16b73e5bd585ea1636b2740354a827
Side-On <nathanhighams@gmail.com>
2021-05-04 18:20:38 +0000

    Fix issue where button wasn't closing when the user clicks off from it

--- static/js/script.js
+++ static/js/script.js
@@ -9,8 +9,7 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left',
-      hoverEnabled: false
+      direction: 'left'
     });
   });
   
