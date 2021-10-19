3e61684e68dae2d08df2b5e7f3bff149c323253d
Side-On <nathanhighams@gmail.com>
2021-10-19 19:58:27 +0000

    Turn on testing as error is now showing on heroku

--- app.py
+++ app.py
@@ -340,4 +340,4 @@ def search_jokes():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=False)
+            debug=True)
